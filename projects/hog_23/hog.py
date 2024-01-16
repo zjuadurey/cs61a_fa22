34f89ab77ec07ad7ed0c0967135d65abd9dea515
adurey_laptop_lenove_yoga_14c <1721966349@qq.com>
2024-01-16 17:42:42 +0800

    problem 10

--- projects/hog_23/hog.py
+++ projects/hog_23/hog.py
@@ -329,7 +329,10 @@ def boar_strategy(score, opponent_score, threshold=11, num_rolls=6):
     points, and returns NUM_ROLLS otherwise. Ignore score and Sus Fuss.
     """
     # BEGIN PROBLEM 10
-    return num_rolls  # Remove this line once implemented.
+    if boar_brawl(score, opponent_score) >= threshold:
+        return 0
+   
+    return num_rolls  
     # END PROBLEM 10
 
 
