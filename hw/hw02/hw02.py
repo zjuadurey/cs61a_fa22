07d6d1e5b5cd1c3593a14da860fe044e05a49a7c
adurey_laptop_lenove_yoga_14c <1721966349@qq.com>
2024-01-17 11:19:52 +0800

    Q2: Accumulate

--- hw/hw02/hw02.py
+++ hw/hw02/hw02.py
@@ -31,7 +31,6 @@ def product(n, term):
     >>> product(3, triple)    # 1*3 * 2*3 * 3*3
     162
     """
-    "*** YOUR CODE HERE ***"
     sum = 1
     for i in range(1, n + 1):
         sum *= term(i)
@@ -62,7 +61,10 @@ def accumulate(merger, start, n, term):
     >>> accumulate(lambda x, y: (x + y) % 17, 19, 20, square)
     16
     """
-    "*** YOUR CODE HERE ***"
+    result = start
+    for i in range(1, n + 1):
+        result = merger(result, term(i))
+    return result
 
 
 def summation_using_accumulate(n, term):
@@ -79,7 +81,7 @@ def summation_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    "*** YOUR CODE HERE ***"
+    return accumulate(add, 0, n, term)
 
 
 def product_using_accumulate(n, term):
@@ -96,7 +98,7 @@ def product_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    "*** YOUR CODE HERE ***"
+    return accumulate(mul, 1, n, term)
 
 
 def funception(func1, begin):
